8d26950ccc6d5b0df917652e272eb64333646807
azon <madscwn@gmail.com>
2021-07-31 17:05:01 +0200

    Added replay-button to reload the scene

--- SetGame/Assets/Scripts/UIManager.cs
+++ SetGame/Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -136,5 +137,10 @@ public class UIManager : MonoBehaviour
         {
             SetButtonsInteractable(true);
         }
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
